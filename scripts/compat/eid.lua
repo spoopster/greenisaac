a5cfa19bc9d408745e0cc7de2a0d19276dc9b918
spoop <robyfiru70@gmail.com>
2023-08-27 10:08:03 +0300

    fuck you broken g6

--- scripts/compat/eid.lua
+++ scripts/compat/eid.lua
@@ -708,7 +708,7 @@ if EID then
         [cards.BROKEN_DICE] = {
             Name = "Broken G6",
             Desc = {
-                "All items in the room turn into 4-6 greencoin, each of the greencoin has a 50% chance to instead be a Pear",
+                "All pickups in the room turn into greencoin, depending on how valuable the pickup is, and each of the greencoin has a 50% chance to instead be a Pear",
                 "If greencoin aren't unlocked and none of the players are Green Cain, they're replaced by random coins",
                 "If Pears aren't unlocked and none of the players are a green character, they're replaced by random pickups",
             },
